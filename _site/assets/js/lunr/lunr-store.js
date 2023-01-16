4f1400422c147cc6c30f5652d999b2c80785e285
paul <paul@system76-pc.localdomain>
2023-01-16 17:36:29 -0500

    text

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "EC70",
-        "excerpt":"Ip Camera  ","categories": [],
+        "title": "TP-Link Kasa Indoor Pan/Tilt Smart Security Camera EC70",
+        "excerpt":"Reverse Engineering the EC70 IP Camera   I decided I wanted to start looking into IP cameras and this one was relatively cheap and had good reviews so I figured it was a good place to start.  ","categories": [],
         "tags": [],
         "url": "/EC70/",
         "teaser": null
